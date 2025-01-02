1906c066bde418df4ef6683c84d16efd62222eef
Bryan Gomez <bryanyupi@hotmail.com>
2025-01-02 17:55:28 -0600

    Primer vistazo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>And here we go! Y AQUÍ VAMOS HIJUESUPUTAMADREEEEEEEEEEE</Text>
       <StatusBar style="auto" />
     </View>
   );
